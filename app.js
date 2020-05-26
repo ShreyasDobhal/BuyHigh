40a9667854230b5f373de47e0ff25433539f1b50
ShreyasDobhal <shreyasdobhal@gmail.com>
2020-05-26 15:13:30 +0530

    Created routes handler

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = express();
 const port = 4000;
 const dbName = 'buyhigh';
 
-const dataJSON = JSON.parse(fs.readFileSync('routes.json'));
-console.log(dataJSON);
+// const dataJSON = JSON.parse(fs.readFileSync('routes.json'));
+// console.log(dataJSON);
 
 let Product = require('./models/product');
 
@@ -23,16 +23,6 @@ db.once('open',function(){
     // Connected
     console.log("We are connected !");
 });
-
-
-// var contactSchema = new mongoose.Schema({
-//     name: String,
-//     phone: String,
-//     email: String,
-//     address: String
-// });
-// var Contact = mongoose.model('Contact', contactSchema);
-
 
 // MORGAN
 app.use(morgan('dev'));
@@ -53,61 +43,41 @@ app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
 
 
-// Express Session
-app.use(session({
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: true,
-    // cookie: {secure:true}
-}));
-
-// Express Messages
-app.use(require('connect-flash')());
-app.use(function(req,res,next){
-    res.locals.messages = require('express-messages')(req,res);
-    next();
-});
-
-// app.use(expressValidate);
-
-// Express Validator 
-// app.use(expressValidator({
-//     errorFormatter: function(param,msg,value) {
-//         var namespace = param.split('.')
-//         , root = namespace.shift()
-//         , formParam = root;
-
-//         while(namespace.length) {
-//             formParam = '['+namespace.shift()+']';
-//         }
-//         return {
-//             param: formParam,
-//             msg: msg,
-//             value: value
-//         };
-//     }
+// // Express Session
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: true,
+//     saveUninitialized: true,
+//     // cookie: {secure:true}
 // }));
 
+// // Express Messages
+// app.use(require('connect-flash')());
+// app.use(function(req,res,next){
+//     res.locals.messages = require('express-messages')(req,res);
+//     next();
+// });
 
-// Routes
+
+// ROUTES 
+// /articles
 let articles = require('./routes/articles.js');
 app.use('/articles',articles);
+// /products
+let products = require('./routes/products.js');
+app.use('/products',products);
+
 
 // END POINTS
 
 app.get('/',(req,res)=>{
-    // Simple text on webpage
-    // res.send('Server is running . . . ');
-    // res.send(dataJSON);
-    res.flash('success','Server running');
-    res.render('layout.pug');
+    res.send('Server is running . . . ');
 });
 
 app.get('/home',(req,res)=>{
     // Sending parameters and rendering a webpage
     const params = {
-        title:'BuyHigh',
-        routes: dataJSON
+        title:'BuyHigh'
     };
     res.status(200).render('home.pug',params);
 });
@@ -119,151 +89,151 @@ app.get('/json',(req,res)=>{
     });
 });
 
-app.get('/products/:proId',(req,res)=>{
-    // Receiving query
-    Product.findById(req.params.proId,function(err,product){
-        if (err) {
-            console.log("Error in searching");
-            return;
-        } else {
-            console.log(product);
-            let products = [product]
-            res.status(200).render('products.pug',{
-                products: products
-            });
-        }
-    });
-});
+// app.get('/products/:proId',(req,res)=>{
+//     // Receiving query
+//     Product.findById(req.params.proId,function(err,product){
+//         if (err) {
+//             console.log("Error in searching");
+//             return;
+//         } else {
+//             console.log(product);
+//             let products = [product]
+//             res.status(200).render('products.pug',{
+//                 products: products
+//             });
+//         }
+//     });
+// });
 
-app.get('/products/edit/:proId',(req,res)=>{
-    // Receiving query
-    Product.findById(req.params.proId,function(err,product){
-        if (err) {
-            console.log("Error in searching");
-            return;
-        } else {
-            console.log(product);
-            // let products = [product]
-            res.status(200).render('edit-product.pug',{
-                product: product
-            });
-        }
-    });
-});
+// app.get('/products/edit/:proId',(req,res)=>{
+//     // Receiving query
+//     Product.findById(req.params.proId,function(err,product){
+//         if (err) {
+//             console.log("Error in searching");
+//             return;
+//         } else {
+//             console.log(product);
+//             // let products = [product]
+//             res.status(200).render('edit-product.pug',{
+//                 product: product
+//             });
+//         }
+//     });
+// });
 
-app.post('/edit/product/:proId',(req,res)=>{
-    // console.log(req);
-    console.log("Request Body");
-    console.log(req.body);
-    let product = {};
-    // product._id = req.params._id;
-    product.title = req.body.title;
-    product.author = req.body.author;
-    product.body = req.body.description;
-    product.status = req.body.status;
+// app.post('/edit/product/:proId',(req,res)=>{
+//     // console.log(req);
+//     console.log("Request Body");
+//     console.log(req.body);
+//     let product = {};
+//     // product._id = req.params._id;
+//     product.title = req.body.title;
+//     product.author = req.body.author;
+//     product.body = req.body.description;
+//     product.status = req.body.status;
 
-    Product.updateOne({'_id':req.params.proId},product,(err,product)=>{
-        if (err) {
-            console.log("Error in updating");
-            console.log(err);
-            return;
-        } else {
-            res.redirect('/products');
-        }
-    });
-    // res.status(200).send('Product added successfully');
-});
+//     Product.updateOne({'_id':req.params.proId},product,(err,product)=>{
+//         if (err) {
+//             console.log("Error in updating");
+//             console.log(err);
+//             return;
+//         } else {
+//             res.redirect('/products');
+//         }
+//     });
+//     // res.status(200).send('Product added successfully');
+// });
 
-app.delete('/delete/product/:proId',(req,res)=>{
-    // console.log(req);
-    console.log("Request Body");
-    console.log(req.body);
-    // let product = {};
-    // product._id = req.params._id;
-    // product.title = req.body.title;
-    // product.author = req.body.author;
-    // product.body = req.body.description;
-    // product.status = req.body.status;
+// app.delete('/delete/product/:proId',(req,res)=>{
+//     // console.log(req);
+//     console.log("Request Body");
+//     console.log(req.body);
+//     // let product = {};
+//     // product._id = req.params._id;
+//     // product.title = req.body.title;
+//     // product.author = req.body.author;
+//     // product.body = req.body.description;
+//     // product.status = req.body.status;
 
-    Product.remove({'_id':req.params.proId},(err,product)=>{
-        if (err) {
-            console.log("Error in deleting");
-            console.log(err);
-            return;
-        } else {
-            res.redirect('/products');
-        }
-    });
-    // res.status(200).send('Product added successfully');
-});
+//     Product.remove({'_id':req.params.proId},(err,product)=>{
+//         if (err) {
+//             console.log("Error in deleting");
+//             console.log(err);
+//             return;
+//         } else {
+//             res.redirect('/products');
+//         }
+//     });
+//     // res.status(200).send('Product added successfully');
+// });
 
 
-app.get('/products',(req,res)=>{
-    console.log("Products searched");
-    Product.find({}, function(err,products){
-        if (err) {
-            console.log("Error");
-            console.log(err);
-            res.send("Error");
-        } else {
-            console.log("Products are :");
-            console.log(products);
-            res.status(200).render('products.pug',{
-                products: products
-            });
-        }
-    });
-});
+// app.get('/products',(req,res)=>{
+//     console.log("Products searched");
+//     Product.find({}, function(err,products){
+//         if (err) {
+//             console.log("Error");
+//             console.log(err);
+//             res.send("Error");
+//         } else {
+//             console.log("Products are :");
+//             console.log(products);
+//             res.status(200).render('products.pug',{
+//                 products: products
+//             });
+//         }
+//     });
+// });
 
-app.get('/addproduct',(req,res)=>{
-    res.status(200).render('add-product.pug');
-});
+// app.get('/addproduct',(req,res)=>{
+//     res.status(200).render('add-product.pug');
+// });
 
-app.post('/addproduct',(req,res)=>{
-    req.checkBody('title','Title is required').notEmpty();
+// app.post('/addproduct',(req,res)=>{
+//     req.checkBody('title','Title is required').notEmpty();
     
-    let errors = req.validationErrors();
+//     let errors = req.validationErrors();
 
-    if (errors) {
-        console.log("Error found");
-        res.render('add-product.pug');
-    } else {
-        console.log("Worked fine");
-    }
-    // console.log(req);
-    console.log("Request Body");
-    console.log(req.body);
-    let product = new Product();
-    product.title = req.body.title;
-    product.author = req.body.author;
-    product.body = req.body.description;
-    product.status = req.body.status;
+//     if (errors) {
+//         console.log("Error found");
+//         res.render('add-product.pug');
+//     } else {
+//         console.log("Worked fine");
+//     }
+//     // console.log(req);
+//     console.log("Request Body");
+//     console.log(req.body);
+//     let product = new Product();
+//     product.title = req.body.title;
+//     product.author = req.body.author;
+//     product.body = req.body.description;
+//     product.status = req.body.status;
 
-    product.save((err,product)=>{
-        if (err) {
-            console.log("Error in saving");
-            console.log(err);
-            return;
-        } else {
-            res.redirect('/products');
-        }
-    });
-    // res.status(200).send('Product added successfully');
-});
+//     product.save((err,product)=>{
+//         if (err) {
+//             console.log("Error in saving");
+//             console.log(err);
+//             return;
+//         } else {
+//             res.redirect('/products');
+//         }
+//     });
+//     // res.status(200).send('Product added successfully');
+// });
 
 
 
-app.post('/products',(req,res)=>{
-    // Receiving a payload
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-    res.status(200).json({
-        message: 'Product added',
-        product: product
-    })
-});
+// app.post('/products',(req,res)=>{
+//     // Receiving a payload
+//     const product = {
+//         name: req.body.name,
+//         price: req.body.price
+//     };
+//     res.status(200).json({
+//         message: 'Product added',
+//         product: product
+//     })
+// });
 
 app.post('/save',(req,res)=>{
     // Saving data to MongoDB
---
+++ routes/products.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const router = express.Router();
+
+let Product = require('../models/product');
+
+router.get('/',(req,res)=>{
+    // View all the products
+    console.log("Products searched");
+    Product.find({}, function(err,products){
+        if (err) {
+            console.log("Error in displaying all products");
+            console.log(err);
+            res.send("Error");
+        } else {
+            console.log("Products are :");
+            console.log(products);
+            res.status(200).render('products.pug',{
+                products: products
+            });
+        }
+    });
+});
+
+router.get('/view/:proId',(req,res)=>{
+    // View product with given id
+    Product.findById(req.params.proId,function(err,product){
+        if (err) {
+            console.log("Error in searching");
+            return;
+        } else {
+            console.log(product);
+            let products = [product]
+            res.status(200).render('products.pug',{
+                products: products
+            });
+        }
+    });
+});
+
+router.get('/add',(req,res)=>{
+    // Add a product
+    res.status(200).render('add-product.pug');
+});
+
+router.post('/add',(req,res)=>{
+    // POST method to handle add request
+    console.log("Request Body");
+    console.log(req.body);
+    let product = new Product();
+    product.title = req.body.title;
+    product.author = req.body.author;
+    product.body = req.body.description;
+    product.status = req.body.status;
+
+    product.save((err,product)=>{
+        if (err) {
+            console.log("Error in adding new product");
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/products');
+        }
+    });
+});
+
+router.get('/edit/:proId',(req,res)=>{
+    // Edit product with given id
+    Product.findById(req.params.proId,function(err,product){
+        if (err) {
+            console.log("Error in searching");
+            return;
+        } else {
+            console.log(product);
+            // let products = [product]
+            res.status(200).render('edit-product.pug',{
+                product: product
+            });
+        }
+    });
+});
+
+router.post('/edit/:proId',(req,res)=>{
+    // Post method to handle edit request
+    console.log("Request Body");
+    console.log(req.body);
+    let product = {};
+    product.title = req.body.title;
+    product.author = req.body.author;
+    product.body = req.body.description;
+    product.status = req.body.status;
+
+    Product.updateOne({'_id':req.params.proId},product,(err,product)=>{
+        if (err) {
+            console.log("Error in updating");
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/products');
+        }
+    });
+});
+
+app.delete('/delete/:proId',(req,res)=>{
+    // DELETE method to handle delete request
+    console.log("Request Body");
+    console.log(req.body);
+
+    Product.remove({'_id':req.params.proId},(err,product)=>{
+        if (err) {
+            console.log("Error in deleting");
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/products');
+        }
+    });
+});
+
+
+
+
+
+
+
+module.exports = router;
